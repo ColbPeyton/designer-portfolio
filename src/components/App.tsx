7b66a4d2cc4e8079f994c8e22c4014ae99d8bb15
Colby Peyton <colby.peyton@gmail.com>
2021-04-03 18:12:06 -0500

    add content to header. Merge to add detect screen resize functionality to app component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import {Header} from './Header';
 import {About} from './About';
 import {Banner} from './Banner';
 import {WorkContainer} from './WorkContainer';
 
-export class App extends React.Component{
-    render(){
-        return(
-            <div>
-                <Header/>
-                <About />
-                <Banner title={'work'} />
-                <WorkContainer screenSize={'mobile'}/>
-                <Banner />
-            </div>
-        )
+export const App = ():JSX.Element => {
+    const [width, setWidth] = useState(window.innerWidth);
+
+    // Header will change based on size of screen, detect change in width
+    useEffect(()=>{
+        window.addEventListener("resize", updateWidth);
+        return () => {
+          window.removeEventListener("resize", updateWidth);
+        }
+    })
+  
+    function updateWidth(){
+      setWidth(window.innerWidth);
     }
+    return(
+        <div>
+            <Header width={width}/>
+            <About />
+            <Banner title={'work'} />
+            <WorkContainer screenSize={'mobile'}/>
+            <Banner />
+        </div>
+    )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,44 @@
 import React from 'react';
 
-export const Header = ():JSX.Element => {
+interface HeaderProps{
+    width: number;
+}
+
+export const Header = (props: HeaderProps):JSX.Element => {
+
+    const mobileHeader = ():JSX.Element => {
+        return (
+            <div className='header-mobile'>
+                <p>Logo</p>
+            </div>
+        )
+    }
+
+    const desktopHeader = ():JSX.Element => {
+        return (
+            <div className= 'header-desktop'>
+                <div className='logo'>
+                    <p>Logo</p>
+                </div>
+                <nav className='links'>
+                    <a href='#container-about'>about</a>
+                    <a href='#container-work'>work</a>
+                    <a href='#container-contact'>contact</a>
+                </nav>
+            </div>
+        )
+    }
+
+    const renderHeaderBasedOnScreenSize = ():JSX.Element => {
+        return props.width <= 480 
+            ? mobileHeader() 
+            : desktopHeader();
+    }
+
+
     return(
-        <div>
-            Header
+        <div className='container-header'>
+            {renderHeaderBasedOnScreenSize()}
         </div>
     )
 }
