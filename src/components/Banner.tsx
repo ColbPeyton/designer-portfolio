3d0fac38fde1a099406823999b272699027cea4a
ColbPeyton <33738479+ColbPeyton@users.noreply.github.com>
2021-04-19 15:41:24 -0500

    update Banner to pure component. add basic styling to Banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
-
+import '../styles/Banner.scss';
 
 interface BannerProps{
     title?: string;
 }
 
-export const Banner = (props: BannerProps): JSX.Element => {
+export const Banner = React.memo((props: BannerProps): JSX.Element => {
+
+    const renderText = ():JSX.Element => {
+        return props.title
+        ? <h1>{props.title}</h1>
+        : <div></div>
+    }
+
     return(
-        <div>
-            {props.title ? props.title : null}
+        <div className='container-banner'>
+            <div className='banner-text'>
+                {renderText()}
+            </div>
         </div>
     )
-}
+});
