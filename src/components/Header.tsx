b2b39e6005d47530c87c8e453722a18de0555e52
ColbPeyton <33738479+ColbPeyton@users.noreply.github.com>
2021-05-12 17:04:05 -0500

    add state to header, add useEffect method to render bee icon after certain YOffset. include basic mobile styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React,{useContext} from 'react';
+import React,{useContext, useState, useEffect} from 'react';
 import {WidthContext} from '../contexts/WidthContext';
 import beeIcon from '../assets/branding/Bee Icon.png';
 import logo from '../assets/branding/Pixel Bee Logo.png';
@@ -9,16 +9,41 @@ interface HeaderProps{
     width: number;
 }
 
+interface HeaderState{
+    height: number;
+}
+
 export const Header = ():JSX.Element => {
 
     // TODO: add mobile button to scroll to top of page. 
 
     const width = useContext<HeaderProps['width']>(WidthContext);
+    const [height, setHeight] = useState<HeaderState['height']>(window.pageYOffset);
+
+    const updateHeight = () =>{
+        setHeight(window.pageYOffset);
+    }
+
+    useEffect(()=>{
+        window.addEventListener("scroll", updateHeight);
+        return () => {
+          window.removeEventListener("scroll", updateHeight);
+        }
+    })
+  
+    const renderButtonAtYOffset = ():string => {
+        return height >= 150 
+        ? 'active'
+        : ''
+    }
+
 
     const mobileHeader = ():JSX.Element => {
         return (
             <div className='header-mobile'>
-                <img src={beeIcon} alt='bee' className='bee-icon'/>
+                <button className={`bee-container ${renderButtonAtYOffset()}`} onClick={()=> {window.scrollTo({top: 0, behavior: 'smooth'})}}>
+                    <img src={beeIcon} alt='bee'/>
+                </button>
                 <div className='logo-container'>
                     <img src={logo} alt='Pixel Bee Banner' />
                 </div>
