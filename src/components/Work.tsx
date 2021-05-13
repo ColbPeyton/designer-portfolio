7c30f64666a639d068d01a62338ab9f230acc156
ColbPeyton <33738479+ColbPeyton@users.noreply.github.com>
2021-05-13 00:51:54 -0500

    add simple-react-lightbox context to app. include type declaration file for lightbox. implement lightbox into work component

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import {dataInfo} from '../_data/data';
+import { SRLWrapper } from "simple-react-lightbox";
 
 interface WorkProps{
     project: dataInfo
 }
 
 export const Work = (props: WorkProps):JSX.Element => {
+    
+    const updateStyleOnProp = ():string => {
+        return props.project.isSquare
+        ? 'square'
+        : ''
+    }
+
+
+
     return(
-        <button className='container-work'>
-            <img src={props.project.thumbnail.default} alt={props.project.name} />
-        </button>
+        <SRLWrapper>
+            <button className={`container-work ${updateStyleOnProp()}`}>
+                <img src={props.project.thumbnail.default} alt={props.project.name} />
+            </button>
+        </SRLWrapper>
     )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './components/App';
+import SimpleReactLightbox from 'simple-react-lightbox';
 import './reset.scss';
 import './styles/index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SimpleReactLightbox>
+      <App />
+    </SimpleReactLightbox>
   </React.StrictMode>,
   document.getElementById('root')
 );
